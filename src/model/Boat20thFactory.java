96450c85fb83d7bbf8438ba7a126439e8144ae57
Thiebaud Marbach <thiebaud.marbach2@etu.univ-lorraine.fr>
2018-11-20 12:00:32 +0100

    jsépa

--- src/model/Boat20thFactory.java
+++ src/model/Boat20thFactory.java
@@ -7,12 +7,13 @@ public class Boat20thFactory implements BoatFactory {
 	@Override
 	public ArrayList<Boat> creerBateaux() {
 		ArrayList<Boat> a = new ArrayList<Boat>();
-		int[] images = {0,1,2,3,4,5};
+		int[] images = {0,1,6,3,4,5};
 		a.add(new Boat20th("Porte-Avions", 1, 5, images));
-		a.add(new Boat20th("Croiseur", 1, 4, images));
-		a.add(new Boat20th("Contre-Torpilleur", 1, 3, images));
-		a.add(new Boat20th("Sous-Marin", 1, 3, images));
-		a.add(new Boat20th("Torpilleur", 1, 2, images));
+		int[] images2 = {0,1,2,3,4,5};
+		a.add(new Boat20th("Croiseur", 1, 4, images2));
+		a.add(new Boat20th("Contre-Torpilleur", 1, 3, images2));
+		a.add(new Boat20th("Sous-Marin", 1, 3, images2));
+		a.add(new Boat20th("Torpilleur", 1, 2, images2));
 		return a;
 	}
 	
